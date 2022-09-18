6049e67f09f60630a7d54e3d502b86b819834942
ariefgivarp <ariefgivarp@gmail.com>
2022-09-18 21:42:42 +0700

    tugas async

--- promise.js
+++ promise.js
@@ -1,8 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
 
+  const theaters = [...theaterIXX, ...theaterVGC];
+  let count = 0;
+  theaters.forEach((item) => {
+    if(item.hasil === emosi){
+      count++;
+    }
+  })
+  return count;
+};
 module.exports = {
   promiseOutput,
 };
